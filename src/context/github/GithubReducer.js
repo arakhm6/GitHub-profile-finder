9cd46f8bf8d36298b8701de507edcc73f922d4e4
arakhm6 <arakhm6@gmail.com>
2022-05-02 21:08:14 -0400

    fetch users function

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -6,6 +6,12 @@ const githubReducer = (state, action) => {
         users: action.payload,
         loading: false,
       };
+      break;
+    case "SET_LOADING":
+      return {
+        ...state,
+        loading: true,
+      };
     default:
       return state;
   }
