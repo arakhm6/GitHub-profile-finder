0ec9bd91af1897fe712d6abba4505e5ec85eedff
arakhm6 <arakhm6@gmail.com>
2022-05-10 14:17:13 -0400

    user profile update

--- src/components/pages/User.js
+++ src/components/pages/User.js
@@ -1,10 +1,10 @@
 import { FaCodepen, FaStore, FaUserFriends, FaUsers } from 'react-icons/fa'
 import { useEffect, useContext } from 'react'
-import { Link, useParams } from 'react-router-dom'
-import Spinner from '../layout/Spinner'
-import GithubContext from '../../context/github/GithubContext'
-import RepoList from '../repos/RepoList'
-import { getUserAndRepos } from '../../context/github/GithubActions'
+import { useParams, Link } from 'react-router-dom'
+import Spinner from '../../components/layout/Spinner'
+import RepoList from '../../components/repos/RepoList'
+import GithubContext from '../context/github/GithubContext'
+import { getUserAndRepos } from '../context/github/GithubActions'
 
 function User() {
   const { user, loading, repos, dispatch } = useContext(GithubContext)
@@ -50,6 +50,7 @@ function User() {
             Back To Search
           </Link>
         </div>
+
         <div className='grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8'>
           <div className='custom-card-image mb-6 md:mb-0'>
             <div className='rounded-lg shadow-xl card image-full'>
@@ -58,10 +59,11 @@ function User() {
               </figure>
               <div className='card-body justify-end'>
                 <h2 className='card-title mb-0'>{name}</h2>
-                <p>{login}</p>
+                <p className='flex-grow-0'>{login}</p>
               </div>
             </div>
           </div>
+
           <div className='col-span-2'>
             <div className='mb-6'>
               <h1 className='text-3xl card-title'>
@@ -76,13 +78,14 @@ function User() {
                 <a
                   href={html_url}
                   target='_blank'
-                  rel='noreferrel'
+                  rel='noreferrer'
                   className='btn btn-outline'
                 >
-                  Visit GitHub Profile
+                  Visit Github Profile
                 </a>
               </div>
             </div>
+
             <div className='w-full rounded-lg shadow-md bg-base-100 stats'>
               {location && (
                 <div className='stat'>
@@ -94,12 +97,8 @@ function User() {
                 <div className='stat'>
                   <div className='stat-title text-md'>Website</div>
                   <div className='text-lg stat-value'>
-                    <a
-                      href={`httpsL//${blog}`}
-                      target='_blank'
-                      rel='noreferrer'
-                    >
-                      {blog}
+                    <a href={websiteUrl} target='_blank' rel='noreferrer'>
+                      {websiteUrl}
                     </a>
                   </div>
                 </div>
@@ -123,43 +122,49 @@ function User() {
         </div>
 
         <div className='w-full py-5 mb-6 rounded-lg shadow-md bg-base-100 stats'>
-          <div className='stat'>
-            <div className='stat-figure text-secondary'>
-              <FaUsers className='text-3xl md:text-5xl' />
+          <div className='grid grid-cols-1 md:grid-cols-3'>
+            <div className='stat'>
+              <div className='stat-figure text-secondary'>
+                <FaUsers className='text-3xl md:text-5xl' />
+              </div>
+              <div className='stat-title pr-5'>Followers</div>
+              <div className='stat-value pr-5 text-3xl md:text-4xl'>
+                {followers}
+              </div>
             </div>
-            <div className='stat-title pr-5'>Followers</div>
-            <div className='stat-value pr-5 text-3xl md:text-4xl'>
-              {followers}
+
+            <div className='stat'>
+              <div className='stat-figure text-secondary'>
+                <FaUserFriends className='text-3xl md:text-5xl' />
+              </div>
+              <div className='stat-title pr-5'>Following</div>
+              <div className='stat-value pr-5 text-3xl md:text-4xl'>
+                {following}
+              </div>
             </div>
-          </div>
-          <div className='stat'>
-            <div className='stat-figure text-secondary'>
-              <FaUserFriends className='text-3xl md:text-5xl' />
+
+            <div className='stat'>
+              <div className='stat-figure text-secondary'>
+                <FaCodepen className='text-3xl md:text-5xl' />
+              </div>
+              <div className='stat-title pr-5'>Public Repos</div>
+              <div className='stat-value pr-5 text-3xl md:text-4xl'>
+                {public_repos}
+              </div>
             </div>
-            <div className='stat-title pr-5'>Following</div>
-            <div className='stat-value pr-5 text-3xl md:text-4xl'>
-              {following}
-            </div>
-          </div>
-          <div className='stat'>
-            <div className='stat-figure text-secondary'>
-              <FaCodepen className='text-3xl md:text-5xl' />
-            </div>
-            <div className='stat-title pr-5'>Public Repos</div>
-            <div className='stat-value pr-5 text-3xl md:text-4xl'>
-              {public_repos}
-            </div>
-          </div>
-          <div className='stat'>
-            <div className='stat-figure text-secondary'>
-              <FaStore className='text-3xl md:text-5xl' />
-            </div>
-            <div className='stat-title pr-5'>Public Gists</div>
-            <div className='stat-value pr-5 text-3xl md:text-4xl'>
-              {public_gists}
+
+            <div className='stat'>
+              <div className='stat-figure text-secondary'>
+                <FaStore className='text-3xl md:text-5xl' />
+              </div>
+              <div className='stat-title pr-5'>Public Gists</div>
+              <div className='stat-value pr-5 text-3xl md:text-4xl'>
+                {public_gists}
+              </div>
             </div>
           </div>
         </div>
+
         <RepoList repos={repos} />
       </div>
     </>
